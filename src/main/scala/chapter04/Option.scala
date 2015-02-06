79f50fd63c8364251fc225b56d2e0330fbfb8eb0
Bjørn Hamre <bha@ambita.com>
2015-02-06 09:51:16 +0100

    Temp

---
+++ src/main/scala/chapter04/Option.scala
@@ -0,0 +1,22 @@
+package chapter04
+trait Option[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
+
+}
+
+case object None extends Option[Nothing]{
+  override def map[B](f: (Nothing) => B): Option[B] = None
+  override def flatMap[B](f: (Nothing) => Option[B]): Option[B] = None
+  override def filter(f: (Nothing) => Boolean): Option[Nothing] = None
+  override def getOrElse[B >: Nothing](default: => B): B = default
+  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = None
+}
+
+
+
+
+
