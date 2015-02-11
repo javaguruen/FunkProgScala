7e95828cee0f26e8279f1db4165674f0d0fd8f86
Bjørn Hamre <javaguruen@gmail.com>
2015-02-11 23:19:18 +0100

    Implemented functions

--- src/main/scala/chapter04/Option.scala
+++ src/main/scala/chapter04/Option.scala
@@ -1,15 +1,32 @@
 package chapter04
 
 sealed trait Option[+A] {
+
   def map[B](f: A => B): Option[B] = this match {
     case None => None
-    case Some(a:A) => Some(f(a))
+    case Some(a: A) => Some(f(a))
   }
 
-    def flatMap[B](f: A => Option[B]): Option[B] = None
-    def getOrElse[B >: A](default: => B): B = default
-    def orElse[B >: A](ob: => Option[B]): Option[B] = ob
-    def filter(f: A => Boolean): Option[A] = None
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => ob
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case None => None
+    case Some(a) => ma
+  }
 }
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
